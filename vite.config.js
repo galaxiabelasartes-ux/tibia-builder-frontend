b952070e64518f9fe5b877eff16c8aeb63b7d74b
galaxiabelasartes-ux <galaxiabelasartes@gmail.com>
2025-08-31 20:40:09 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["react-dnd", "react-dnd-html5-backend"],
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
 });
